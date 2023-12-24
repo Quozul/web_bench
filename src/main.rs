f74726fd6065845dd07971cb9b919b994e01ba70
Erwan <30729291+Quozul@users.noreply.github.com>
2023-12-24 14:56:12 +0100

    plot threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use plotters::prelude::*;
 use std::ops::Add;
 use std::time::Duration;
 
@@ -13,30 +14,60 @@ async fn bench(requests: u32) -> Duration {
     sum
 }
 
-async fn request(threads: u32, total_requests: u32) {
-    let requests_per_thread = total_requests / threads;
-
-    let tasks = (0..threads).map(|_| tokio::spawn(bench(requests_per_thread)));
-    let awaited = futures::future::join_all(tasks).await;
-
-    let mut sum = Duration::new(0, 0);
-
-    for duration in awaited.into_iter().flatten() {
-        sum = sum.add(duration);
-    }
-
-    let average_response_time = sum / total_requests;
-    println!(
-        "Threads: {}, Total Requests: {}, Average: {:?}",
-        threads, total_requests, average_response_time
-    );
-}
-
 #[tokio::main]
-async fn main() {
-    for requests in 7..10 {
-        for threads in 0..5 {
-            request(2_u32.pow(threads), 2_u32.pow(requests)).await;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let threads_counts = vec![1, 2, 4, 8, 16];
+    let request_counts = vec![500, 1000, 2000, 4000];
+
+    let (min_threads, max_threads) = (1u32, 16);
+
+    let root = BitMapBackend::new("output.png", (800, 600)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Average Response Time", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(min_threads..max_threads, 0..100u128)?;
+
+    chart.configure_mesh().draw()?;
+
+    for total_requests in request_counts {
+        let mut response_times = Vec::new();
+
+        for threads in threads_counts.clone() {
+            let requests_per_thread = total_requests / threads;
+
+            let tasks = (0..threads).map(|_| tokio::spawn(bench(requests_per_thread)));
+            let awaited = futures::future::join_all(tasks).await;
+
+            let mut sum = Duration::new(0, 0);
+
+            for duration in awaited.into_iter().flatten() {
+                sum = sum.add(duration);
+            }
+
+            let average_response_time = sum / total_requests;
+            println!(
+                "Threads: {}, Total Requests: {}, Average: {:?}",
+                threads, total_requests, average_response_time
+            );
+            response_times.push((threads, average_response_time.as_millis()));
         }
+
+        chart
+            .draw_series(LineSeries::new(response_times, &RED))?
+            .label(format!("{total_requests} requests"))
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
     }
+
+    chart
+        .configure_series_labels()
+        .background_style(WHITE.mix(0.8))
+        .border_style(BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
 }
